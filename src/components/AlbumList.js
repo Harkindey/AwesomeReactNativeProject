6f09934baed542149b96eed865b2d99a69c40cb7
Emmanuel AKINDE <ibukunbamise@yahoo.com>
2017-04-18 18:55:22 +0100

    Moved to Remote Repo

--- src/components/AlbumList.js
+++ src/components/AlbumList.js
@@ -1,7 +1,16 @@
 import React, { Component } from 'react';
 import { Text, View } from 'react-native';
+import axios from 'axios';
 
 class AlbumList extends Component {
+state = {};
+
+  componentWillMount() {
+    axios.get('https://rallycoding.herokuapp.com/api/music_albums').then((res) => {
+      console.log(res);
+    });
+  }
+
   render() {
     return (
       <View >
