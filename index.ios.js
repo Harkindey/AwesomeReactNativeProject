b26ed2dc1d18ac8f5c447cb314f48809d9a82027
Emmanuel AKINDE <ibukunbamise@yahoo.com>
2017-04-18 17:32:42 +0100

    rendering Functional Component

--- index.ios.js
+++ index.ios.js
@@ -2,13 +2,17 @@
 
 // import a library to helo create a Component
 import React from 'react';
-import { AppRegistry } from 'react-native';
-import Header from './src/components/header';
+import { AppRegistry, View } from 'react-native';
+import Header from './src/components/Header';
+import AlbumList from './src/components/AlbumList';
 
 
 // create a component
 const App = () => (
+  <View>
     <Header headerText={'Albums'} />
+    <AlbumList />
+  </View>
 );
 
 // render the component to the device
