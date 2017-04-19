4d4d7ab2a95124536f765318b0c11d274d60ae17
Emmanuel AKINDE <ibukunbamise@yahoo.com>
2017-04-19 16:36:58 +0100

    Using dedicated album componnent

--- src/components/AlbumList.js
+++ src/components/AlbumList.js
@@ -1,21 +1,30 @@
 import React, { Component } from 'react';
-import { Text, View } from 'react-native';
+import { View } from 'react-native';
 import axios from 'axios';
+import AlbumDetail from './AlbumDetails';
 
 class AlbumList extends Component {
 state = { albums: [] };
+// Use STATE only with class based component
 
   componentWillMount() {
     axios.get('https://rallycoding.herokuapp.com/api/music_albums')
-    .then(res => this.setState({ albums: res.data }))
+    .then(res => this.setState({ albums: res.data }));
+  }
+
+  renderAlbums() {
+    return this.state.albums.map(album =>
+      <AlbumDetail key={album.title} record={album} />);
   }
 
   render() {
     console.log(this.state);
+    // PROP --> communiation btw a parent component to child component
+    // STATE -->  for component internal recored keeping
 
     return (
       <View >
-      <Text>Album List !!! </Text>
+        {this.renderAlbums()}
       </View>
     );
   }
