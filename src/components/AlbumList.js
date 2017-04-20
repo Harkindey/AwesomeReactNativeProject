76b3d60a51976e48f4eb10b72d3b66746a7b491f
Emmanuel AKINDE <ibukunbamise@yahoo.com>
2017-04-20 15:52:35 +0100

    making component scrollable

--- src/components/AlbumList.js
+++ src/components/AlbumList.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { View } from 'react-native';
+import { ScrollView } from 'react-native';
 import axios from 'axios';
 import AlbumDetail from './AlbumDetails';
 
@@ -23,9 +23,9 @@ state = { albums: [] };
     // STATE -->  for component internal recored keeping
 
     return (
-      <View >
+      <ScrollView >
         {this.renderAlbums()}
-      </View>
+      </ScrollView>
     );
   }
 }
